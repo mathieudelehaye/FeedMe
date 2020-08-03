2d7f76afe3f588cac43ef85e45ad5a16e8ea749d
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2020-08-03 07:41:47 +0200

    Images changed to add pre-processed effects Real day image added to project

--- FeedMe/Views/DayCell.swift
+++ FeedMe/Views/DayCell.swift
@@ -26,37 +26,10 @@ class DayCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        // Get image rendering context
-        let context = CIContext();
-        
-        // Convert image to CIImage
-        let aUIImage = dayImage.image;
-        guard let aCGImage = aUIImage?.cgImage else { fatalError("Image cannot be extracted from Image View") }
-        var aCIImage = CIImage(cgImage: aCGImage)
-        let originalImageExtent = aCIImage.extent // Save orginal image extent for final cropping
-        
-        // Apply brightness filter
-        guard let brightnessFilter = CIFilter(name: "CIColorControls") else { fatalError("Cannot create color filter") }
-        brightnessFilter.setValue(aCIImage, forKey: kCIInputImageKey)
-        brightnessFilter.setValue(NSNumber(value: -0.26), forKey: kCIInputBrightnessKey);
-        guard let colouredImage = brightnessFilter.outputImage else { fatalError("Cannot apply color filter") }
-        aCIImage = colouredImage
-        
-        // Apply blur filter
-        guard let gaussianFilter = CIFilter(name: "CIGaussianBlur") else { fatalError("Cannot create blur filter") }
-        gaussianFilter.setValue(aCIImage, forKey: kCIInputImageKey)
-        gaussianFilter.setValue(3.1, forKey: kCIInputRadiusKey)
-        guard let blurredImage = gaussianFilter.outputImage else { fatalError("Cannot convert back image from filter")  }
-        aCIImage = blurredImage
-        
-        // Display image
-        guard let imageRef = context.createCGImage(aCIImage, from: originalImageExtent) else { fatalError("Cannot convert image to CG format") }
-        dayImage.image = UIImage(cgImage: imageRef)
-                        
-        // Round image corners
+                  
+        // Calculate radius
         let radius = dayImage.frame.size.height / 15
-        dayImage.layer.cornerRadius = radius
+        
         // Round coloured transparent view corners
         let colourViewBounds = colouredView.bounds
         let path = UIBezierPath(roundedRect: colourViewBounds, byRoundingCorners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: radius, height: radius))
@@ -65,12 +38,11 @@ class DayCell: UITableViewCell {
         colouredView.layer.mask = mask
 
         // Add shadow to image super view
-        imageSuperView.layer.cornerRadius = imageSuperView.frame.size.height / 15
         imageSuperView.layer.shadowColor = UIColor.black.cgColor
         imageSuperView.layer.shadowOffset = CGSize(width: 0, height: 1)
         imageSuperView.layer.shadowOpacity = 0.25
         imageSuperView.layer.shadowRadius = 4.0
-        imageSuperView.clipsToBounds = false        
+        imageSuperView.clipsToBounds = false
     }
         
     @IBAction func editButtonPressed(_ sender: UIButton) {
