74aa0393ceac76aea30baa902594ec90ff9c82ba
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2020-08-06 07:33:52 +0200

    Derived class MealViewController inherits from ListViewController Meal View Controller customised in Storyboard

--- FeedMe/Controllers/MealViewController.swift
+++ FeedMe/Controllers/MealViewController.swift
@@ -8,34 +8,98 @@
 
 import UIKit
 
-class MealViewController: UITableViewController {
+class MealViewController: ListViewController, UITableViewDelegate {
 
     var selectedDay : Day?
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self // delegate for table view data source
+        
+        tableView.delegate = self   // delegate for table view events
+        
+        tableView.register(UINib(nibName: K.mealCellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)    // register custom cell to table view
+        
+        loadMeals()  // read meals from realm DB and load table view
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    //MARK: - Data Manipulation Methods
+       
+    func loadMeals() {
+
+        let mealArray = realm.objects(Meal.self)
         
-        title = selectedDay!.name
+        itemArray = []
+        for meal in mealArray {
+            itemArray.append(meal)
+        }
+        
+        tableView.reloadData()
         
     }
 
-    //MARK: - Tableview Datasource Methods
-    // Return the number of rows for the table.
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+}
 
+//MARK: - TableView Data Source Methods
+
+extension MealViewController {
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) 
-                                
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! CustomCell
+    
+        let item = itemArray[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        cell.editor = self
+        cell.row = indexPath.row
+
         return cell
     }
     
 }
 
+//MARK: - ViewController Cell Edition Delegate Methods
+extension MealViewController {
+        
+    override func showEditionView(forCellAtRow cellRow: Int) {
+           
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let evc = storyboard.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
+        
+        let selectedCellItem = itemArray[cellRow]
+        
+        evc.selectedItem = selectedCellItem
+        
+        updateRemainingItems(keepingItem: selectedCellItem.name, fromStartList: [ "Breakfast", "Dinner", "Supper" ])
+        
+        presentModal(itemNames: remainingItems, forViewController: evc)
+        
+    }
+    
+}
+
+//MARK: - ViewController Cell Edition Delegate Methods
+extension MealViewController {
+    
+    override func updateCBView() {
+        
+        loadMeals()
+    }
+    
+    override func manageCBView(withObjectName objectName: String) {
+        
+        let newDay = Day()
+        
+        newDay.name = objectName
+        
+        save(item: newDay)
+        
+        loadMeals()
+        
+    }
+}
